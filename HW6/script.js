1305dde66c96eae48847d361102ada132b74d903
Mariy <m.lebadina@gmail.com>
2022-04-06 20:56:20 +0300

    commit HW6

---
+++ HW6/script.js
@@ -0,0 +1,48 @@
+function func1() {
+
+}
+function func2() {
+    let date = prompt('Введите дату в формате гггг-мм-дд');
+    let dateFormatted = date.split('-').reverse().join('/');
+    console.log(dateFormatted)
+}
+function func3() {
+    let str = 'Я учу javascript';
+    console.log(str.substring(2, 5));
+    console.log(str.substr(2, 3));
+    console.log(str.slice(2, 5));
+}
+function func4() {
+    let arr = [4, 2, 5, 19, 13, 0, 10];
+    let sum = 0;
+    arr.forEach(elem => {
+        sum += Math.pow(elem, 3);
+    });
+    console.log(Math.pow(sum, 1/2));
+}
+function func5() {
+    let a = +prompt('Введите a');
+    let b = +prompt('Введите b');
+    let c = Math.abs(a - b);
+    console.log(`Модуль (${a} - ${b}) = ${c}`);
+}
+function func6() {
+    let date = new Date();
+    let timeFormatted = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':' + (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+    let dateFormatted = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + '.' + (date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)) + '.' + date.getFullYear();
+    console.log(timeFormatted, ' ', dateFormatted);
+}
+function func7() {
+    let str = 'aa aba abba abbba abca abea';
+    console.log(str.match(/ab+a/g));
+}
+function func8() {
+    let phoneNum = prompt('Введите номер телефона в формате +ххх хх ххххххх');
+    const phoneReg = /^\+[1-9]{3} [1-9]{2} [1-9]{3}[1-9]{2}[1-9]{2}$/g;
+    console.log(phoneReg.test(phoneNum));
+}
+function func9() {
+    let email = prompt('Введите адрес электронной почты');
+    const emailReg = /^[a-z0-9]+([.-_]{1}[a-z0-9]+)*@{1}(\w+\.)+[a-z0-9]{2,11}$/i;
+    console.log(emailReg.test(email));
+}
