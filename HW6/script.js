6ea6959607f25f1e9268189301671ac2b94affa9
Mariy <m.lebadina@gmail.com>
2022-04-11 13:29:43 +0300

    ++HW6

--- HW6/script.js
+++ HW6/script.js
@@ -4,7 +4,8 @@ function func1() {
 }
 function func2() {
     let date = prompt('Введите дату в формате гггг-мм-дд');
-    let dateFormatted = date.split('-').reverse().join('/');
+   // let dateFormatted = date.split('-').reverse().join('/');
+    let dateFormatted = date.replace(/(\d{4})\-(\d{2})\-(\d{2})/, '$3/$2/$1');
     console.log(dateFormatted)
 }
 function func3() {
@@ -38,12 +39,28 @@ function func7() {
     console.log(str.match(/ab+a/g));
 }
 function func8() {
-    let phoneNum = prompt('Введите номер телефона в формате +ххх хх ххххххх');
-    const phoneReg = /^\+[1-9]{3} [1-9]{2} [1-9]{3}[1-9]{2}[1-9]{2}$/g;
-    console.log(phoneReg.test(phoneNum));
+    let phoneNum = prompt('Введите номер телефона в формате');
+    const phoneReg = /^(3\d{11}$)|(8\d{10}$)/g;
+    let phone = phoneNum.match(/\d{1,12}/g).join('');
+    console.log(phone, phoneReg.test(phone));
 }
 function func9() {
     let email = prompt('Введите адрес электронной почты');
     const emailReg = /^[a-z0-9]+([.-_]{1}[a-z0-9]+)*@{1}(\w+\.)+[a-z0-9]{2,11}$/i;
     console.log(emailReg.test(email));
+}
+function func10() {
+  // let url = 'https://tech.onliner.by/2018/04/26/smart-do-200/?utm_source=main_tile&utm_medium=smartdo200#zag3';
+  //  let url = 'https://www.google.com/search?q=github&oq=github+&aqs=chrome..69i57j69i60l4.4517j0j7&sourceid=chrome&ie=UTF-8';
+    let url = 'https://ru.stackoverflow.com/questions/488935/regexp-%D1%81%D0%BE%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%81%D1%82%D0%B2%D1%83%D1%8E%D1%89%D0%B8%D0%B9-%D0%BB%D1%8E%D0%B1%D0%BE%D0%BC%D1%83-%D0%BD%D0%B0%D0%B1%D0%BE%D1%80%D1%83-%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB%D0%BE%D0%B2-%D0%B4%D0%BE-%D0%BF%D0%B5%D1%80%D0%B2%D0%BE%D0%B3%D0%BE-%D0%B2%D1%85%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F-%D0%BF%D0%BE%D0%B4%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B8';
+    
+    let urlDomReg = /https{0,1}\:\/\/(\w+\.)+[a-z]{2,11}/,
+        urlMethodReg = /(.*)(?=\?|$)/,
+        urlHashReg = /\#\w+/;
+    let urlDom = url.match(urlDomReg)[0],
+        urlMethod = url.slice(urlDom.length).match(urlMethodReg)[0],
+        urlHash = url.match(urlHashReg) ? url.match(urlHashReg)[0] : '';
+        urlParams = urlHash ? url.slice((urlDom.length + urlMethod.length), (url.length - urlHash.length)) : url.slice((urlDom.length + urlMethod.length), url.length)
+    let arr = [urlDom, urlMethod, urlParams, urlHash];
+    console.log(arr);
 }
